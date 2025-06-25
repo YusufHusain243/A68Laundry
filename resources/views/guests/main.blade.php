2893d4e4a16d549ffe6229e24c6bb987d3705890
upttik58 <upttik58@gmail.com>
2025-06-25 20:00:20 +0700

    a

--- resources/views/guests/main.blade.php
+++ resources/views/guests/main.blade.php
@@ -24,11 +24,11 @@
 
 <body>
     <!-- loader-wrapper -->
-    <div class="loader-wrapper">
+    {{-- <div class="loader-wrapper">
         <div class="spinner-border text-primary p-5" role="status">
             <span class="visually-hidden">Loading...</span>
         </div>
-    </div>
+    </div> --}}
 
     @include('guests.components.navbar')
     @include('guests.components.banner')
